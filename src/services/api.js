2e2ce0b6efc2c5d6fe589361fceb5a177301bb34
parshyagk23 <gkprashant52@gmail.com>
2025-12-30 13:01:36 +0530

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://bc2ccourses-backend.onrender.com",
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://bc2ccourses-backend.onrender.com",
+    // baseURL: "http://127.0.0.1:8000",
     headers: {
         "Content-Type": "application/json",
     },
